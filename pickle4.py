909a89b18425df71a6bdbc6103b67b4924e16331
Mohammed Muzzammil <36696171+mohammed-muzzammil@users.noreply.github.com>
2022-04-24 18:19:27 +0530

    Add files via upload

---
+++ pickle4.py
@@ -0,0 +1,24 @@
+import pickle
+import streamlit as st
+import os
+
+# Header
+st.title("Salary prediction model with pickle")
+
+# "/app/{repository name}/ {file.extension}"
+Pkl_Filename = "/app/pickle_demo_streamlit/Model.pkl"
+
+# Import the machine learning model using pickle
+with open(Pkl_Filename, 'rb') as f:
+    regressor = pickle.load(f)
+
+
+# Function to predict salary based on experience
+
+yoe = st.slider('Years of Experience', 0, 10)
+if st.button('Predict'):
+    salary = regressor.predict([[yoe]])
+
+    st.success('Salary is $ {}'.format(salary))
+
+
